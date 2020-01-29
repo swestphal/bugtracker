8e3387895851b8e0f06e0388ee5c7c5abd3a5d5d
Simone-Westphal <sw@thinkweb360.de>
2020-01-29 19:35:15 +0100

    Add values for xdebug in order to show more results

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
     declare(strict_types=1);
 
+    // xdebug settings
+    ini_set('xdebug.var_display_max_depth', '10');
+    ini_set('xdebug.var_display_max_children', '256');
+    ini_set('xdebug.var_display_max_data', '12288');
+
     require_once __DIR__ . '/vendor/autoload.php';
 
+    set_exception_handler([new \App\Exception\ExceptionHandler(), 'handle']);
+    $config = \App\Helpers\Config::getFileContent('jipiyaya');
+
+
+    var_dump($config);
 
     $config = \App\Helpers\Config::get('app', 'app_name');
     $application = new \App\Helpers\App;
